35aab75430fe428531d13cb67be4bf3ce7b08fef
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-26 21:28:27 +0000

    CHnaged a lot of code

--- models/base_model.py
+++ models/base_model.py
@@ -6,30 +6,30 @@ from datetime import datetime, timezone
 
 class BaseModel():
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key in ('created_at', 'updated_at'):
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
 
     def __str__(self):
         return f'[{self.__class__.__name__}]({self.id}){self.__dict__}'
 
     def save(self):
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
 
-    def to_dict(self):
-        self.__dict__.update(
-            {'__class__': __class__.__name__,
-            'updated_at': self.updated_at.isoformat(),
-            'created_at': self.created_at.isoformat()
-            })
-        return self.__dict__
-        # sorted_items = sorted(self.__dict__.items())
-        # return dict(sorted_items)
+        def to_dict(self):
+            new_dict = self.__dict__.copy()
+            new_dict["__class__"] = type(self).__name__
+            new_dict["created_at"] = new_dict["created_at"].isoformat()
+            new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+            return new_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import json
 
+
 class FileStorage():
     __file_path = 'filestoragedb.json'
     __objects = dict()  # __class__.__name__ + <object> + .id: <class name>.id
@@ -15,5 +16,15 @@ class FileStorage():
         obj_dict = dict()
         for obj_key, obj_value in self.__objects.items():
             obj_dict[obj_key] = obj_value.to_dict()
-        with open(self.__file_path, 'w') as json_file:
+        with open(self.__file_path, 'w', encoding="utf-8") as json_file:
             json.dump(obj_dict, json_file)
+
+    def reload(self):
+        try:
+           with open(self.__file_path, 'r') as fhand:
+                json_dict = json.loads(fhand.read())
+                for value in json_dict.values():
+                    cls = value["__class__"]
+                    self.new(eval(cls)(**value))
+        except Exception:
+            pass
