57c7659090ade5d2ce42b64a413c953bacc0aab7
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-27 06:02:58 +0000

    pycodestyle checked

--- console.py
+++ console.py
@@ -24,7 +24,7 @@ class HBNBCommand(cmd.Cmd):
         prompt (str): CLI prompt displayed to the user.
     """
 
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_EOF(self, line):
         """ Exit the CLI on EOF command. """
