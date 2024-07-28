a1aa55fe56bbdec0d5f484c0cf29b09bf728304c
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-28 03:07:11 +0000

    added docstrings, corrected password attr typo

--- models/user.py
+++ models/user.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""
+This module defines the User class.
+The User class inherits from BaseModel and includes additional attributes.
+"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """
+    User class that inherits from BaseModel.
+
+    Attributes:
+        email (str): Email of the user. Default is an empty string.
+        password (str): Password of the user. Default is an empty string.
+        first_name (str): First name of the user. Default is an empty string.
+        last_name (str): Last name of the user. Default is an empty string.
+    """
     email = ""
-    pasword = ""
+    password = ""
     first_name = ""
     last_name = ""
