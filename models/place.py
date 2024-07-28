9c3fea12832592d9a841cb4df7271f612eb3dd54
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-28 03:56:25 +0000

    corrected places.py attr name

--- models/place.py
+++ models/place.py
@@ -31,7 +31,7 @@ class Place(BaseModel):
     description: str = ""
     number_rooms: int = 0
     number_bathrooms: int = 0
-    max_gue: int = 0
+    max_guest: int = 0
     price_by_night = 0
     latitude: float = 0.0
     longitude: float = 0.0
